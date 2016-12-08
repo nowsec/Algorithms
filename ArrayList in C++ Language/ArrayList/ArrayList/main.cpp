e7b4d08492eee8c235c8cea1dc5926347b1076e7
gess <nowsec@nate.com>
2016-12-08 16:16:02 +0900

    Algorithm ver 0.1
    ADD:
    ArrayList

---
+++ ArrayList in C++ Language/ArrayList/ArrayList/main.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "ArrayList.h"
+
+int main()
+{
+	List list;
+	int data;
+	ListInit(&list);
+
+	LInsert(&list, 11); LInsert(&list, 22);
+	LInsert(&list, 33); LInsert(&list, 44);
+	LInsert(&list, 55);
+
+	printf("Current Data Count : %d \n", LCount(&list));
+	while (LNext(&list, &data))
+		printf("%d ", data);
+	if (LFirst(&list, &data))
+	{
+		printf("%d ", data);
+
+		while (LNext(&list, &data))
+			printf("%d ", data);
+	}
+	printf("\n\n");
+
+	if (LFirst(&list, &data))
+	{
+		if (data == 22)
+			LRemove(&list);
+
+		while (LNext(&list, &data))
+		{
+			if (data == 22)
+				LRemove(&list);
+		}
+
+	}
+
+	printf("Current Data Count : %d \n", LCount(&list));
+
+	if (LFirst(&list, &data))
+	{
+		printf("%d ", data);
+
+		while (LNext(&list, &data))
+			printf("%d ", data);
+	}
+
+	printf("\n\n");
+	return 0;
+}
